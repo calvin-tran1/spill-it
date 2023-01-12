8aa0e2390001b6e600c118b4a6f194dc64e68bf8
Calvin Tran <calvinxtran@gmail.com>
2023-01-12 20:39:10 +0000

    searchResults state is reset if input field is empty

--- client/components/search-bar.jsx
+++ client/components/search-bar.jsx
@@ -1,6 +1,32 @@
 import React from 'react';
 
 export default class DesktopSearchbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: null,
+      searchResults: [],
+      keystroke: ''
+    };
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleSearch(e) {
+
+    this.setState({ keystroke: e.target.value });
+    const token = window.localStorage.getItem('jwt');
+    const req = {
+      headers: {
+        'X-Access-Token': token
+      }
+    };
+
+    fetch(`/api/search/?users=${this.state.keystroke}`, req)
+      .then(res => res.json())
+      .then(searchResult =>
+        this.setState({ searchResults: e.target.value ? searchResult : [] }));
+  }
+
   render() {
     return (
       <div className="searchbar-wrapper">
@@ -8,8 +34,11 @@ export default class DesktopSearchbar extends React.Component {
           <button type="submit" className="search-btn">
             <i className="fa-solid fa-magnifying-glass" />
           </button>
-          <input type="text" placeholder="Search" name="searchbar" />
+          <input type="text" placeholder="Search" name="searchbar" onChange={this.handleSearch}/>
         </form>
+        <div className="search-results-container">
+          <a>test</a>
+        </div>
       </div>
     );
   }
