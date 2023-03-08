0db41bcc87780e7942f04062e8d63ef4123bd38b
Calvin Tran <calvinxtran@gmail.com>
2023-03-08 20:49:05 +0000

    updated schema file and db

--- database/schema.sql
+++ database/schema.sql
@@ -32,32 +32,7 @@ CREATE TABLE "public"."likes" (
     "likesId" serial NOT NULL,
     "userId" integer NOT NULL,
     "postId" integer default null,
-    "reviewId" integer default null,
-    "commentId" integer default null,
     CONSTRAINT "likes_pk" PRIMARY KEY ("likesId")
-) WITH (
-  OIDS=FALSE
-);
-CREATE TABLE "public"."comments" (
-    "commentId" serial NOT NULL,
-    "userId" integer NOT NULL,
-    "textContent" TEXT,
-    "image" TEXT,
-    "createdAt" timestamptz NOT NULL default now(),
-    CONSTRAINT "comments_pk" PRIMARY KEY ("commentId")
-) WITH (
-  OIDS=FALSE
-);
-CREATE TABLE "public"."reviews" (
-    "reviewId" serial NOT NULL,
-    "userId" integer NOT NULL,
-    "storeName" TEXT NOT NULL,
-    "location" TEXT NOT NULL,
-    "rating" integer NOT NULL,
-    "image" TEXT NOT NULL,
-    "review" TEXT NOT NULL,
-    "createdAt" timestamptz NOT NULL default now(),
-    CONSTRAINT "reviews_pk" PRIMARY KEY ("reviewId")
 ) WITH (
   OIDS=FALSE
 );
@@ -68,15 +43,21 @@ CREATE TABLE "public"."following" (
 ) WITH (
   OIDS=FALSE
 );
+CREATE TABLE "public"."shares" (
+	"sharesId" serial NOT NULL,
+	"userId" integer NOT NULL,
+	"postId" integer NOT NULL,
+	CONSTRAINT "shares_pk" PRIMARY KEY ("sharesId")
+) WITH (
+  OIDS=FALSE
+);
 ALTER TABLE "posts" ADD CONSTRAINT "posts_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
 ALTER TABLE "posts" ADD CONSTRAINT "posts_fk1" FOREIGN KEY ("username") REFERENCES "users"("username");
 ALTER TABLE "posts" ADD CONSTRAINT "posts_fk2" FOREIGN KEY ("displayName") REFERENCES "users"("displayName");
 ALTER TABLE "posts" ADD CONSTRAINT "posts_fk3" FOREIGN KEY ("avatar") REFERENCES "users"("image");
 ALTER TABLE "likes" ADD CONSTRAINT "likes_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
 ALTER TABLE "likes" ADD CONSTRAINT "likes_fk1" FOREIGN KEY ("postId") REFERENCES "posts"("postId");
-ALTER TABLE "likes" ADD CONSTRAINT "likes_fk2" FOREIGN KEY ("reviewId") REFERENCES "reviews"("reviewId");
-ALTER TABLE "likes" ADD CONSTRAINT "likes_fk3" FOREIGN KEY ("commentId") REFERENCES "comments"("commentId");
-ALTER TABLE "comments" ADD CONSTRAINT "comments_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
-ALTER TABLE "reviews" ADD CONSTRAINT "reviews_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
 ALTER TABLE "following" ADD CONSTRAINT "following_fk0" FOREIGN KEY ("followingId") REFERENCES "users"("userId");
 ALTER TABLE "following" ADD CONSTRAINT "following_fk1" FOREIGN KEY ("userId") REFERENCES "users"("userId");
+ALTER TABLE "shares" ADD CONSTRAINT "shares_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
+ALTER TABLE "shares" ADD CONSTRAINT "shares_fk1" FOREIGN KEY ("postId") REFERENCES "posts"("postId");
